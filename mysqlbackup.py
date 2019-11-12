e19a417454aef04eb79d9a68ceac79c7f20cc308
root <edvinas.boguckis@gmail.com>
2019-11-12 11:42:25 +0200

    initial

--- mysqlbackup.py
+++ mysqlbackup.py
@@ -1,61 +1,58 @@
 import os
+import sys
 import time
 import datetime
 import pipes
-import sys
+import configparser
 
-print ("Usage: backup.py {host} {db name} {db user} {db user password} {backup path}")
+runparam = sys.argv[1]
 
-VERSION = 1.1
-PARAM = sys.argv[1]
-DB_HOST = sys.argv[1]
-DB_NAME = sys.argv[2]
-DB_USER = sys.argv[3]
-DB_USER_PASSWORD = sys.argv[4]
-BACKUP_PATH = sys.argv[5]
-ADD_ARG = sys.argv[6]
-#if (PARAM == "info"):
-#    print ("Mysqlbackuper "+VERSION)
-print ("============================================================================")
-print ("Using current settings: ")
-print ("Database Host: " + DB_HOST)
-print ("Database Name: " + DB_NAME)
-print ("Database User: " + DB_USER)
-print ("Database User Password: " + DB_USER_PASSWORD)
-print ("Backup directory: " + BACKUP_PATH)
-DATETIME = time.strftime('%Y%m%d-%H%M%S')
-FULLBACKUPPATH = BACKUP_PATH + '/' + DATETIME
-print ("Full Path: " + FULLBACKUPPATH)
-print ("============================================================================")
+def backuper (db_host, db_name, db_user, db_pass, backup_path, arc_arg, hide_pass):
+    backupdate = time.strftime('%Y%m%d-%H%M%S')
+    fullpath = backup_path + '/' + backupdate
+    
 
-try:
-    print ("Checking if root backup folder exists...")
-    os.stat(BACKUP_PATH)
-except:
-    try:
-        print ("Root backup folder does not exist... creating...")
-        os.mkdir(BACKUP_PATH)
-    except:
-        print ("Folder already exists, skipping")
-os.mkdir(FULLBACKUPPATH)
-print ("Creating full backup folder")
+def parseconfig(filelocation):
+    localconf = configparser.ConfigParser()
+    localconf.read(filelocation)
+    db_host = localconf['backuper']['db_host']
+    db_name = localconf['backuper']['db_name']
+    db_user = localconf['backuper']['db_user']
+    db_pass = localconf['backuper']['db_pass']
+    backup_path = localconf['backuper']['path']
+    arc_arg = localconf['backuper']['archive']
+    hide_pass = localconf['extras']['hide_pass']
+    backuper()
+    
 
-db = DB_NAME
-dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(FULLBACKUPPATH) + "/" + db + ".sql"
-os.system(dumpcmd)
-if (ADD_ARG == "--noarchive"):
-        print ("==got nozip argument, skipping archivation==")
-        print ("============================================================================")
-        print ("Backup script completed")
-        print ("Backup has been created in '" + FULLBACKUPPATH + "' directory")
-        exit()
-elif (ADD_ARG == "--archive"):
-        gzipcmd = "gzip " + pipes.quote(FULLBACKUPPATH) + "/" + db + ".sql"
-        os.system(gzipcmd)
-        print ("============================================================================")
-        print ("Backup script completed")
-        print ("Backup has been created in '" + FULLBACKUPPATH + "' directory")
-        exit()
-else:
-        print ("No argument received")
-        exit()
+def initialize():
+    if (runparam == "fromconfig"):
+        location = sys.argv[2]
+        parseconfig(location)
+    elif (runparam == "singlerun"):
+        db_host = sys.argv[2]
+        db_name = sys.argv[3]
+        db_user = sys.argv[4]
+        db_pass = sys.argv[5]
+        backup_path = sys.argv[6]
+        arc_arg = sys.argv[7]
+        hide_pass = False
+        backuper(db_host, db_name, db_user, db_pass, backup_path, arc_arg, hide_pass)
+    elif (runparam == "help"):
+        showhelp()
+    elif (runparam == "info"):
+        printinfo()
+    else:
+        print ("No required arguments provided! Use help parameter for help. \nExiting..")
+        exit(1)
+
+
+
+
+
+
+
+app_version = 1.2
+app_branch = "stage"
+isdev = True
+initialize()
