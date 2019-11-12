80a0f80ad021d634f17b25171d16032f762eccf2
root <edvinas.boguckis@gmail.com>
2019-11-12 12:22:23 +0200

    wip

--- mysqlbackup.py
+++ mysqlbackup.py
@@ -10,7 +10,32 @@ runparam = sys.argv[1]
 def backuper (db_host, db_name, db_user, db_pass, backup_path, arc_arg, hide_pass):
     backupdate = time.strftime('%Y%m%d-%H%M%S')
     fullpath = backup_path + '/' + backupdate
-    
+    try:
+        print ("Checking if root backup folder exists... \n..OK")
+        os.stat(backup_path)
+    except:
+        try:
+            print ("Root backup folder does not exist... creating... \n..OK")
+            os.mkdir(backup_path)
+        except:
+            print ("Folder already exists, skipping")
+    os.mkdir(fullpath)
+    print ("Creating full backup folder \n..OK")
+    db = db_name
+    dumpcmd = "mysqldump -h " + db_host + " -u " + db_user + " -p" + db_pass + " " + db + " > " + pipes.quote(fullpath) + "/" + db + ".sql"
+    os.system(dumpcmd)
+    if (ADD_ARG == "--noarchive"):
+        print ("==got nozip argument, skipping archivation== \n============================================================================ \nBackup script completed \nBackup has been created in '" + FULLBACKUPPATH + "' directory")
+        exit(0)
+    elif (ADD_ARG == "--archive"):
+        gzipcmd = "gzip " + pipes.quote(FULLBACKUPPATH) + "/" + db + ".sql"
+        os.system(gzipcmd)
+        print ("==got archive argument, starting archivation== \n============================================================================ \nBackup script completed \nBackup has been created in '" + FULLBACKUPPATH + "' directory")
+        exit(0)
+    else:
+        print ("No argument received")
+        exit(1)
+    exit(0)
 
 def parseconfig(filelocation):
     localconf = configparser.ConfigParser()
@@ -22,7 +47,7 @@ def parseconfig(filelocation):
     backup_path = localconf['backuper']['path']
     arc_arg = localconf['backuper']['archive']
     hide_pass = localconf['extras']['hide_pass']
-    backuper()
+    backuper(db_host, db_name, db_user, db_pass, backup_path, arc_arg, hide_pass)
     
 
 def initialize():
